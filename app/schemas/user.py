73a02406f41ea1a069303ee8ba36ed1b7607ef63
Imkongxu <imkongxu@outlook.com>
2025-12-02 16:28:16 +0800

    1

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -10,22 +10,3 @@ class Token(BaseModel):
 class TokenData(BaseModel):
     user_id: Optional[int] = None
 
-# --- 用户相关 ---
-class UserBase(BaseModel):
-    email: Optional[EmailStr] = None
-    phone_number: Optional[str] = Field(None, pattern=r"^1[3-9]\d{9}$")
-
-class UserCreate(UserBase):
-    password: str = Field(min_length=8, max_length=20)
-    email: EmailStr # 注册时邮箱必填
-
-class UserLogin(BaseModel):
-    email: EmailStr
-    password: str
-
-class UserResponse(UserBase):
-    id: int # 注意这里对应数据库的 id
-    is_active: bool = True
-    
-    class Config:
-        from_attributes = True
